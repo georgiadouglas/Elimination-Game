60f986d25adab9f6eba9921abae8802673dcace9
georgiadouglas <44413213+georgiadouglas@users.noreply.github.com>
2019-04-12 09:43:21 -0700

    Add files via upload

---
+++ Elimination Game/Elimination Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Elimination Game
+//
+//  Created by Georgia Ann Douglas on 4/1/19.
+//  Copyright © 2019 Georgia Ann Douglas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
